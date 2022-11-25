1638a2f93bb1f63edaba7bb8f25dd6db890ed704
Oliwer Ellréus <oe222ez@student.lnu.se>
2022-11-25 19:37:49 +0100

    Remove output dependency, rename ansible-execution to execute-ansible

--- output.tf
+++ output.tf
@@ -1,8 +1,4 @@
 # Print floating ip
 output "terraform-public-ip" {
   value = openstack_networking_floatingip_v2.terraform-floatingip.address
-
-  depends_on = [
-    openstack_compute_floatingip_associate_v2.terraform-floatingip
-  ]
 }
