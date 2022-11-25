41388beb952dbec4f1853906e6b2a0c073bbc7c1
Oliwer Ellréus <oe222ez@student.lnu.se>
2022-11-25 18:34:56 +0100

    Move index.html, Add ansible playbook, Run ansible using terraform

--- main.tf
+++ main.tf
@@ -110,6 +110,19 @@ resource "openstack_compute_floatingip_associate_v2" "terraform-floatingip" {
   }
 }
 
+resource "local_file" "inventory" {
+  content = "[webservers]\n${openstack_networking_floatingip_v2.terraform-floatingip.address}"
+  filename = "${path.module}/inventory"
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i inventory ./configuration.yml --extra-vars='ansible_ssh_private_key_file=${var.identity_file}'"
+  }
+
+  depends_on = [
+    openstack_compute_floatingip_associate_v2.terraform-floatingip
+  ]
+}
+
 # Print floating ip
 output "terraform-public-ip" {
   value = openstack_networking_floatingip_v2.terraform-floatingip.address
