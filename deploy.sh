6e606d51c45de19afa29abfb8918c286a936c543
Oliwer Ellréus <oe222ez@student.lnu.se>
2022-11-25 19:42:54 +0100

    Add command instructions in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 if [ -z $OS_USERNAME ] || [ -z $OS_PROJECT_NAME ] || [ -z $OS_PASSWORD ] || [ -z $OS_AUTH_URL ]; then
   echo "Log into openstack using your rc file before running this script!"
+  echo "Example: '. my-openrc.sh'"
 else
   if [ -z $TF_VAR_key_name ]; then
     echo "Export variable 'TF_VAR_key_name' before running this script!"
+    echo "Example: 'export TF_VAR_key_name=my-key'"
   elif [ -z $TF_VAR_identity_file ]; then
     echo "Export variable 'TF_VAR_identity_file' before running this script!"
+    echo "Example: 'export TF_VAR_identity_file=~/.ssh/my-key'"
   else
     # Set Terraform variables
     export TF_VAR_os_username=$OS_USERNAME
